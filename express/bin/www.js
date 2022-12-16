4055ffaee2399d4a08703773f69e17fa8a419195
cheongyeong <cheongyeonggg@gmail.com>
2022-12-16 17:47:29 +0900

    push

--- express/bin/www.js
+++ express/bin/www.js
@@ -1,8 +1,6 @@
 let app = require('../app');
 
-let port = process.env.PORT || 8080;
+let port = 8080;
 
 
-app.listen(port, () => {
-  console.log(`${port}로 express 실행`);
-})
+app.listen(process.env.PORT || 8080);
